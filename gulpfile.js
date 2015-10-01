3b8a8a19a7db8ea95370674c1b3d8cc9cc9e5bed
Alexander Löhn <alexloehn@gmail.com>
2015-10-01 21:36:56 +0200

    Skip coveralls
    Skip coveralls because of an error.
    'error from lcovParse: ' 'Failed to parse string'

--- gulpfile.js
+++ gulpfile.js
@@ -9,41 +9,45 @@ var nsp = require('gulp-nsp');
 var plumber = require('gulp-plumber');
 var coveralls = require('gulp-coveralls');
 
-gulp.task('nsp', function (cb) {
-  nsp('package.json', cb);
+gulp.task('nsp', function(cb) {
+	nsp('package.json', cb);
 });
 
-gulp.task('pre-test', function () {
-  return gulp.src('lib\**\*.js')
-    .pipe(istanbul({
-      includeUntested: true
-    }))
-    .pipe(istanbul.hookRequire());
+gulp.task('pre-test', function() {
+	return gulp.src('lib\**\*.js')
+		.pipe(istanbul({
+			includeUntested: true
+		}))
+		.pipe(istanbul.hookRequire());
 });
 
-gulp.task('test', ['pre-test'], function (cb) {
-  var mochaErr;
+gulp.task('test', ['pre-test'], function(cb) {
+	var mochaErr;
 
-  gulp.src('test/**/*.js')
-    .pipe(plumber())
-    .pipe(mocha({reporter: 'spec'}))
-    .on('error', function (err) {
-      mochaErr = err;
-    })
-    .pipe(istanbul.writeReports())
-    .on('end', function () {
-      cb(mochaErr);
-    });
+	gulp.src('test/**/*.js')
+		.pipe(plumber())
+		.pipe(mocha({
+			reporter: 'spec'
+		}))
+		.on('error', function(err) {
+			mochaErr = err;
+		})
+		.pipe(istanbul.writeReports({
+			reporters: ['lcov']
+		}))
+		.on('end', function() {
+			cb(mochaErr);
+		});
 });
 
-gulp.task('coveralls', ['test'], function () {
-  if (!process.env.CI) {
-    return;
-  }
+gulp.task('coveralls', ['test'], function() {
+	if (!process.env.CI) {
+		return;
+	}
 
-  return gulp.src(path.join(__dirname, 'coverage/lcov.info'))
-    .pipe(coveralls());
+	return gulp.src(path.join(__dirname, 'coverage/lcov.info'))
+		.pipe(coveralls()); // Crashes: 'error from lcovParse: ' 'Failed to parse string'
 });
 
 gulp.task('prepublish', ['nsp']);
-gulp.task('default', ['test', 'coveralls']);
+gulp.task('default', ['test']); // + 'coveralls'
